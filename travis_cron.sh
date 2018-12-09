476813f7b52cc4e6f2b836387f2c8045544e9ed1
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-12-09 16:17:04 +0300

    Create travis_cron.sh

---
+++ travis_cron.sh
@@ -0,0 +1,5 @@
+# Take random like from jokes file
+sort --random-sort daily_jokes.txt | head -1 | cat> joke.txt
+git add .
+git commit -m "refresh joke"
+git push --force --quiet "https://$GITHUB_TOKEN@github.com/fiztehradio/daily-jokes.git"
