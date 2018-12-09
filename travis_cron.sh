1af2d07f62ad5f38cc49e67dcb93feb6bf98c9db
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-12-09 16:44:58 +0300

    Update travis_cron.sh

--- travis_cron.sh
+++ travis_cron.sh
@@ -1,5 +1,7 @@
+git checkout master
+
 # Take random like from jokes file
 sort --random-sort daily_jokes.txt | head -1 | cat> joke.txt
 git add .
 git commit -m "refresh joke"
-git push --force --quiet "https://$GITHUB_TOKEN@github.com/fiztehradio/daily-jokes.git"
+git push --force --quiet "https://$GITHUB_TOKEN@github.com/fiztehradio/daily-jokes.git" master:master
