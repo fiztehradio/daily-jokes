67149e3788cbcd3db3a6197cadb8f08c3d953e34
Daniil Okhlopkov <danokhlopkov@gmail.com>
2018-12-09 17:55:09 +0300

    Create send_daily_joke.py

---
+++ send_daily_joke.py
@@ -0,0 +1,21 @@
+import os
+from onesignal import OneSignal, SegmentNotification, Notification
+
+with open('joke.txt', 'r') as f:
+  joke = f.readline()
+
+client = OneSignal(os.environ['ONESIGNAL_APP_ID'], os.environ['ONESIGNAL_API_KEY'])
+notification_to_all_users = SegmentNotification(
+  headings={
+    "en": "Физтех-шутка"
+  },
+  contents={
+    "en": joke
+  },
+  included_segments=SegmentNotification.ALL,
+  url="https://vk.com/radiomipt/",
+  delayed_option=Notification.DELAYED_OPTION_TIMEZONE,
+  send_after="18:00:00",
+)
+
+client.send(notification_to_all_users)
